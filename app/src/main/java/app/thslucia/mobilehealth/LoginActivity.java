93db72bfe1717ca53cee6876287282cc7bdb856f
Lucia Figueroa <lucia.figueroa@towerhousestudio.com>
2017-02-13 17:31:39 -0300

    New login form renamed to LoginActivity

--- app/src/main/java/app/thslucia/mobilehealth/LoginActivity.java
+++ app/src/main/java/app/thslucia/mobilehealth/LoginActivity.java
@@ -3,26 +3,24 @@ package app.thslucia.mobilehealth;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
+import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -33,12 +31,11 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import app.thslucia.mobilehealth.utils.FontManager;
+
 import static android.Manifest.permission.READ_CONTACTS;
 
-/**
- * A login screen that offers login via email/password.
- */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -57,7 +54,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private UserLoginTask mAuthTask = null;
 
-    // UI references.
+    FontManager FM;
+
+    private TextView signIn;
+    private TextView logo;
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
@@ -67,6 +67,20 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FM=new FontManager(getApplicationContext());
+        InitUI();
+        TextView backicon = (TextView)findViewById(R.id.backicon);
+        backicon.setTextColor(Color.parseColor("#ffffff"));
+        FM.setBackIcon(backicon);
+        //When you go back with the back arrow the activity gets closed
+        backicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -75,7 +89,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                if (id == R.id.email_sign_in_button || id == EditorInfo.IME_NULL) {
                     attemptLogin();
                     return true;
                 }
@@ -84,7 +98,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         });
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
@@ -94,6 +108,20 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
+
+    public void InitUI(){
+
+        logo=(TextView)findViewById(R.id.logo);
+        mEmailView=(AutoCompleteTextView)findViewById(R.id.email);
+        mPasswordView=(EditText)findViewById(R.id.password);
+        signIn=(TextView)findViewById(R.id.email_sign_in_button);
+
+        FM.setAppRegular(logo);
+        FM.setAppMedium(mEmailView);
+        FM.setAppMedium(mPasswordView);
+        FM.setAppMedium(signIn);
+    }
+
 
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
@@ -348,5 +376,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
         }
     }
-}
 
+
+
+
+}
